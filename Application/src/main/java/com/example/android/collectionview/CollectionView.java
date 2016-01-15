e60d6f1dc9bcafbcf9115d25d55f925201caabbc
Eric.Liu <L078865@Ws504319.btfin.com>
2016-01-15 16:56:48 +1100

    finishes InventoryGrup

--- Application/src/main/java/com/example/android/collectionview/CollectionView.java
+++ Application/src/main/java/com/example/android/collectionview/CollectionView.java
@@ -6,10 +6,19 @@ import android.util.AttributeSet;
 import android.util.SparseArray;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 /**
  * Created by L078865 on 15/01/2016.
  */
 public class CollectionView extends RecyclerView {
+
+
+    private Inventory mInventory = new Inventory();
+    private CollectionViewCallbacks mCallbacks = null;
+    private MyListAdapter mAdapter = null;
+
+
     public CollectionView(Context context) {
         this(context, null);
     }
@@ -21,10 +30,20 @@ public class CollectionView extends RecyclerView {
 
     public CollectionView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        setAdapter(new MyListAdapter());
+        mAdapter = new MyListAdapter();
+        setAdapter(mAdapter);
 
     }
 
+
+    public void setCollectionAdapter(CollectionViewCallbacks adapter) {
+        mCallbacks = adapter;
+    }
+
+    public void updateInventory(final Inventory inventory) {
+        mInventory = new Inventory(inventory);
+        mAdapter.notifyDataSetChanged();
+    }
 
     protected class MyListAdapter extends RecyclerView.Adapter {
 
@@ -48,7 +67,23 @@ public class CollectionView extends RecyclerView {
     }
 
 
-    public static class InventoryGroup implements Cloneable {
+    private static class RowComputeResult {
+        int row;
+        int groupId;
+        InventoryGroup group;
+        int groupOffset;
+    }
+
+
+    private boolean computeRowContent(int row, RowComputeResult result) {
+        return false;
+    }
+
+
+    /**
+     * Represents a group of items with a header to be displayed in the {@link CollectionView}.
+     */
+    public static class InventoryGroup {
         private int mGroupId = 0;
 
 
@@ -61,6 +96,16 @@ public class CollectionView extends RecyclerView {
 
         public InventoryGroup(int groupId) {
             mGroupId = groupId;
+        }
+
+
+        public InventoryGroup(InventoryGroup copyFrom) {
+            mGroupId = copyFrom.mGroupId;
+            mHeaderItem = copyFrom.mHeaderItem;
+            mDataIndexStart = copyFrom.mDataIndexStart;
+            mItemCount = copyFrom.mItemCount;
+            mItems = copyFrom.cloneSparseArray(copyFrom.mItems);
+            mItemCustomDataIndices = cloneSparseArray(copyFrom.mItemCustomDataIndices);
         }
 
 
@@ -95,8 +140,8 @@ public class CollectionView extends RecyclerView {
         }
 
         public InventoryGroup addItem(Object item) {
+            setItem(mItemCount, item);
             mItemCount++;
-            setItem(mItemCount - 1, item);
             return this;
         }
 
@@ -104,10 +149,69 @@ public class CollectionView extends RecyclerView {
         public int getRowCount() {
             return 1 + mItemCount;
         }
-        
-        
+
+
         public Object getItem(int index) {
             return mItems.get(index, null);
         }
+
+        private static <E> SparseArray<E> cloneSparseArray(SparseArray<E> orig) {
+            SparseArray<E> result = new SparseArray<E>();
+            for (int i = 0; i < orig.size(); i++) {
+                result.put(orig.keyAt(i), orig.valueAt(i));
+            }
+            return result;
+        }
+
+    }
+
+
+    /**
+     * Represents the data of the items to display in the {@link CollectionView}.
+     * This is defined as a list of {@link InventoryGroup} which represents a group of items with a
+     * header.
+     */
+    public static class Inventory {
+        private ArrayList<InventoryGroup> mGroups = new ArrayList<>();
+
+        public Inventory() {
+        }
+
+
+        public Inventory(Inventory copyFrom) {
+            for (InventoryGroup group : copyFrom.mGroups) {
+                mGroups.add(group);
+            }
+        }
+
+        public void addGroup(InventoryGroup group) {
+            if (group.mItemCount > 0) {
+                mGroups.add(new InventoryGroup(group));
+            }
+        }
+
+
+        public int getTotalItemCount() {
+            int total = 0;
+            for (InventoryGroup group : mGroups) {
+                total += group.mItemCount;
+            }
+            return total;
+        }
+
+        public int getGroupCount() {
+            return mGroups.size();
+        }
+
+        public int getGroupIndex(int groupId) {
+            for (int i = 0; i < mGroups.size(); i++) {
+                if (mGroups.get(i).mGroupId == groupId) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+
     }
 }
